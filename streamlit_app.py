38cd404bfb2cb051fe4e4b4c9ab424a957f62dee
yusuf00008 <ysaifutdinov@gmail.com>
2025-07-23 19:08:02 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,14 @@ st.subheader('Случайные 10 срок')
 st.dataframe(df.sample(10), use_container_width = True)
 
 
+st.subheader("📊 Визуализация данных")
+col1, col2 = st.columns(2)
+
+with col1:
+    fig1 = px.histogram(df, x="species", color="island", barmode="group", title="Распределение видов по островам")
+    st.plotly_chart(fig1, use_container_width=True)
+
+with col2:
+    fig2 = px.scatter(df, x="bill_length_mm", y="flipper_length_mm", color="species", title="Длина клюва vs Длина крыла")
+    st.plotly_chart(fig2, use_container_width=True)
+
